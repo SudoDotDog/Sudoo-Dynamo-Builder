a358c6757831f198deb3d353f5b6356fea11a9b6
WMXPY <wm@mengw.io>
2021-07-19 22:07:53 -0500

    add builder unit tests

---
+++ test/unit/builder.test.ts
@@ -0,0 +1,69 @@
+/**
+ * @author WMXPY
+ * @namespace Dynamo_Builder
+ * @description Batch Put
+ * @override Unit Test
+ */
+
+import { expect } from "chai";
+import * as Chance from "chance";
+import { DynamoBatchDeleteBuilder, DynamoBatchPutBuilder, DynamoBuilder, DynamoDeleteBuilder, DynamoGetBuilder, DynamoScanBuilder, DynamoUpdateBuilder } from "../../src";
+
+describe('Given {DynamoBuilder} class', (): void => {
+
+    const chance: Chance.Chance = new Chance('dynamo-builder');
+
+    it('should be able to construct batch delete', (): void => {
+
+        const builder: DynamoBuilder = DynamoBuilder.create(chance.string());
+
+        const batchDeleteBuilder: DynamoBatchDeleteBuilder = builder.batchDelete();
+
+        expect(batchDeleteBuilder).to.be.an.instanceof(DynamoBatchDeleteBuilder);
+    });
+
+    it('should be able to construct batch put', (): void => {
+
+        const builder: DynamoBuilder = DynamoBuilder.create(chance.string());
+
+        const batchPutBuilder: DynamoBatchPutBuilder = builder.batchPut();
+
+        expect(batchPutBuilder).to.be.an.instanceof(DynamoBatchPutBuilder);
+    });
+
+    it('should be able to construct delete', (): void => {
+
+        const builder: DynamoBuilder = DynamoBuilder.create(chance.string());
+
+        const deleteBuilder: DynamoDeleteBuilder = builder.delete();
+
+        expect(deleteBuilder).to.be.an.instanceof(DynamoDeleteBuilder);
+    });
+
+    it('should be able to construct get', (): void => {
+
+        const builder: DynamoBuilder = DynamoBuilder.create(chance.string());
+
+        const getBuilder: DynamoGetBuilder = builder.get();
+
+        expect(getBuilder).to.be.an.instanceof(DynamoGetBuilder);
+    });
+
+    it('should be able to construct scan', (): void => {
+
+        const builder: DynamoBuilder = DynamoBuilder.create(chance.string());
+
+        const scanBuilder: DynamoScanBuilder = builder.scan();
+
+        expect(scanBuilder).to.be.an.instanceof(DynamoScanBuilder);
+    });
+
+    it('should be able to construct update', (): void => {
+
+        const builder: DynamoBuilder = DynamoBuilder.create(chance.string());
+
+        const updateBuilder: DynamoUpdateBuilder = builder.update();
+
+        expect(updateBuilder).to.be.an.instanceof(DynamoUpdateBuilder);
+    });
+});
