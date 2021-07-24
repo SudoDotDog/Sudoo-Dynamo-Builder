c51f7610558adf4c7520804b47b088e687346895
WMXPY <wm@mengw.io>
2021-07-24 10:49:27 -0500

    finsih key expression

--- src/expression/key.ts
+++ src/expression/key.ts
@@ -4,30 +4,7 @@
  * @description Key
  */
 
-import { DynamoRecord } from "../declare";
-import { ExpressionCorrectKeyHandler } from "./correct-key";
+export const buildDynamoKeyExpression = (keys: string[]): string => {
 
-export const buildDynamoKeyExpression = (records: DynamoRecord[]): string => {
-
-    const expressionStack: string[] = [];
-    const correctKeyHandler: ExpressionCorrectKeyHandler = ExpressionCorrectKeyHandler.fromRecords(records);
-
-    for (const record of records) {
-
-        if (typeof record.value !== 'undefined') {
-
-            if (expressionStack.length > 0) {
-                expressionStack.push(', ');
-            }
-
-            const keyKey: string = correctKeyHandler.getCorrectKeyKey(record.key);
-            const valueKey: string = correctKeyHandler.getCorrectValueKey(record.key);
-            expressionStack.push(`${keyKey} = ${valueKey}`);
-        }
-    }
-
-    if (expressionStack.length > 0) {
-        expressionStack.unshift('set ');
-    }
-    return expressionStack.join('');
+    return keys.join(", ");
 };
